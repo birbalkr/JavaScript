1faacff35175acfeae74c2d2a27affd8acf636b1
birbalkr <birbalkr1435@gmail.com>
2023-12-20 18:04:47 +0530

    Day 14: Implemented Toast Notifications in My Project! 🍞✨

---
+++ Toast Notification/script.js
@@ -0,0 +1,25 @@
+
+let toastbox = document.getElementById('toastBox');
+let Successmsg='<i class="fa-solid fa-circle-check"></i> Successfully Submitted';
+let Errrormsg='<i class="fa-solid fa-circle-xmark"></i> Please fix the error!';
+let Invalidmsg='<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again';
+
+
+function showToast(msg){
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML =msg;
+    toastbox.appendChild(toast);
+
+    if (msg.includes('error')) {
+        toast.classList.add('error');
+    }
+
+    if (msg.includes('Invalid')) {
+        toast.classList.add('invalid');
+    }
+
+    setTimeout(()=>{
+        toast.remove();
+    },5000);
+}
